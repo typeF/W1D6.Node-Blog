8c638cc4eb962300e81dcd700d170bc0f1e0671c
Frank Lam <frank.is.missing@gmail.com>
2017-12-03 19:31:07 +0000

    Replaced placeholders

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /* app.js */
 
 // require and instantiate express
-const app = require("express")()
+const app = require('express')()
 
 // fake posts to simulate a database
 const posts = [
   {
     id: 1,
-    author: 'John,'
+    author: 'John',
     title: 'Templating with EJS',
     body: 'Blog post number 1'
   },
@@ -32,23 +32,23 @@ const posts = [
 ]
 
 // set the view engine to ejs
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
 // blog home page
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // render `home.ejs` with the list of posts
   res.render('home', { posts: posts })
 })
 
 // blog post
-app.get("/post/:id", (req, res) => {
+app.get('/post/:id', (req, res) => {
   // find the post in the `posts` array
   const post = posts.filter((post) => {
     return post.id == req.params.id
   })[0]
 
   // render the `post.ejs` template with the post content
-  res.render("post", {
+  res.render('post', {
     author: post.author,
     title: post.title,
     body: post.body
@@ -57,4 +57,4 @@ app.get("/post/:id", (req, res) => {
 
 app.listen(8080)
 
-console.log("listening on port 8080")
+console.log('listening on port 8080')
